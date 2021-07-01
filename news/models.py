f3d485f7296c82d7279f640f4b1a67e41ea328c2
ThyagOliveira <taothyago@gmail.com>
2021-07-01 18:35:50 -0300

    :gem: Create models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Author(models.Model):
+    id = models.UUIDField("ID", primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField("Name", max_length=100)
+    picture = models.URLField("Picture")
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    id = models.UUIDField("ID", primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.CharField("Category", max_length=100)
+    title = models.CharField("Title", max_length=250)
+    summary = models.TextField("Summary", max_length=500)
+    first_paragraph = models.TextField("First paragraph")
+    body = models.TextField("Body")
