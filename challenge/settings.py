d6e40c94d1e7ca520c658e38879a717d9f2c5b39
ThyagOliveira <taothyago@gmail.com>
2021-07-01 20:59:31 -0300

    :wrench: Create docker compose prod file and update docker compose dev

--- challenge/settings.py
+++ challenge/settings.py
@@ -13,16 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import dj_database_url
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ENV_FILE = BASE_DIR.joinpath(".env")
-
-if os.path.isfile(ENV_FILE):
-    from dotenv import load_dotenv
-
-    load_dotenv(ENV_FILE)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
